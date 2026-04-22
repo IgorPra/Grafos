505325d10b17c5d42d20fcaf37f06a76aa03c5c8
Tiago Taumaturgo <tiago.taumaturgo@gmail.com>
2026-04-22 20:08:04 -0300

    .

--- Unidade2/T5/src/GraphColoringDSatur.java
+++ Unidade2/T5/src/GraphColoringDSatur.java
@@ -1,11 +1,21 @@
 public class GraphColoringDSatur {
     private final Graph graph;
+    private final int[] colors;
+    private final int[] coloringOrder;
+    private int coloredCount;
+    private int colorCount;
+    private boolean colored;
 
     public GraphColoringDSatur(Graph graph) {
         if (graph == null) {
             throw new IllegalArgumentException("graph nao pode ser nulo");
         }
         this.graph = graph;
+        this.colors = new int[graph.V()];
+        this.coloringOrder = new int[graph.V()];
+        this.coloredCount = 0;
+        this.colorCount = 0;
+        this.colored = false;
     }
 
     public Graph getGraph() {
@@ -13,26 +23,171 @@ public class GraphColoringDSatur {
     }
 
     public void color() {
-        throw new UnsupportedOperationException("TODO: implementar o algoritmo DSatur");
+        resetColoring();
+
+        if (graph.V() == 0) {
+            colored = true;
+            return;
+        }
+
+        int firstVertex = findHighestDegreeUncoloredVertex();
+        assignColor(firstVertex, 1);
+        colorCount = 1;
+
+        while (coloredCount < graph.V()) {
+            int nextVertex = selectNextVertex();
+            int nextColor = findSmallestFeasibleColor(nextVertex);
+
+            assignColor(nextVertex, nextColor);
+            if (nextColor > colorCount) {
+                colorCount = nextColor;
+            }
+        }
+
+        colored = true;
     }
 
     public int getColor(int vertex) {
-        throw new UnsupportedOperationException("TODO: informar a cor atribuida ao vertice");
+        validateVertex(vertex);
+        return colors[vertex];
     }
 
     public int getColorCount() {
-        throw new UnsupportedOperationException("TODO: informar o total de cores utilizadas");
+        return colorCount;
     }
 
     public int[] getColoringOrder() {
-        throw new UnsupportedOperationException("TODO: informar a ordem de coloracao");
+        int[] order = new int[coloredCount];
+        for (int i = 0; i < coloredCount; i++) {
+            order[i] = coloringOrder[i];
+        }
+        return order;
     }
 
     public boolean isValidColoring() {
-        throw new UnsupportedOperationException("TODO: validar a coloracao produzida");
+        if (!colored || coloredCount != graph.V()) {
+            return false;
+        }
+
+        for (int v = 0; v < graph.V(); v++) {
+            if (colors[v] <= 0) {
+                return false;
+            }
+
+            for (int w : graph.adj(v)) {
+                if (colors[v] == colors[w]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
     }
 
     public String getLabel(int vertex) {
-        throw new UnsupportedOperationException("TODO: mapear o rotulo do vertice para exibicao");
+        validateVertex(vertex);
+        return String.valueOf(vertex);
+    }
+
+    private void resetColoring() {
+        for (int v = 0; v < graph.V(); v++) {
+            colors[v] = 0;
+            coloringOrder[v] = 0;
+        }
+        coloredCount = 0;
+        colorCount = 0;
+        colored = false;
+    }
+
+    private int findHighestDegreeUncoloredVertex() {
+        int bestVertex = -1;
+        int bestDegree = -1;
+
+        for (int v = 0; v < graph.V(); v++) {
+            if (colors[v] != 0) {
+                continue;
+            }
+
+            int degree = graph.degree(v);
+            if (degree > bestDegree || (degree == bestDegree && v < bestVertex)) {
+                bestDegree = degree;
+                bestVertex = v;
+            }
+        }
+
+        return bestVertex;
+    }
+
+    private int selectNextVertex() {
+        int bestVertex = -1;
+        int bestSaturation = -1;
+        int bestDegree = -1;
+
+        for (int v = 0; v < graph.V(); v++) {
+            if (colors[v] != 0) {
+                continue;
+            }
+
+            int saturation = getSaturationDegree(v);
+            int degree = graph.degree(v);
+
+            if (saturation > bestSaturation
+                    || (saturation == bestSaturation && degree > bestDegree)
+                    || (saturation == bestSaturation && degree == bestDegree && v < bestVertex)) {
+                bestVertex = v;
+                bestSaturation = saturation;
+                bestDegree = degree;
+            }
+        }
+
+        return bestVertex;
+    }
+
+    private int getSaturationDegree(int vertex) {
+        boolean[] usedColors = new boolean[colorCount + 1];
+        int saturation = 0;
+
+        for (int neighbor : graph.adj(vertex)) {
+            int neighborColor = colors[neighbor];
+            if (neighborColor > 0 && !usedColors[neighborColor]) {
+                usedColors[neighborColor] = true;
+                saturation++;
+            }
+        }
+
+        return saturation;
+    }
+
+    private int findSmallestFeasibleColor(int vertex) {
+        for (int candidateColor = 1; candidateColor <= colorCount + 1; candidateColor++) {
+            if (isFeasibleColor(vertex, candidateColor)) {
+                return candidateColor;
+            }
+        }
+
+        return colorCount + 1;
+    }
+
+    private boolean isFeasibleColor(int vertex, int candidateColor) {
+        for (int neighbor : graph.adj(vertex)) {
+            if (neighbor != vertex && colors[neighbor] == candidateColor) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void assignColor(int vertex, int color) {
+        colors[vertex] = color;
+        coloringOrder[coloredCount] = vertex;
+        coloredCount++;
+    }
+
+    private void validateVertex(int vertex) {
+        if (vertex < 0 || vertex >= graph.V()) {
+            throw new IllegalArgumentException(
+                    "vertice " + vertex + " nao esta entre 0 e " + (graph.V() - 1)
+            );
+        }
     }
 }
--- Unidade2/T5/src/Main.java
+++ Unidade2/T5/src/Main.java
@@ -14,16 +14,22 @@ public class Main {
         StdOut.println(graph);
         StdOut.println();
 
-        StdOut.println("TODO: implementar a execucao do DSatur.");
-        StdOut.println("TODO: exibir a ordem de coloracao.");
-        StdOut.println("TODO: exibir as cores finais de cada vertice.");
-        StdOut.println("TODO: exibir o total de cores utilizadas.");
-        StdOut.println("TODO: validar se a coloracao produzida eh valida.");
+        dsatur.color();
 
-        // Exemplo de chamadas esperadas apos a implementacao:
-        // dsatur.color();
-        // int[] order = dsatur.getColoringOrder();
-        // int totalColors = dsatur.getColorCount();
-        // boolean valid = dsatur.isValidColoring();
+        StdOut.println("Ordem de coloracao:");
+        int[] order = dsatur.getColoringOrder();
+        for (int i = 0; i < order.length; i++) {
+            StdOut.println((i + 1) + ": vertice " + dsatur.getLabel(order[i]));
+        }
+        StdOut.println();
+
+        StdOut.println("Cores finais dos vertices:");
+        for (int v = 0; v < graph.V(); v++) {
+            StdOut.println("vertice " + dsatur.getLabel(v) + " -> cor " + dsatur.getColor(v));
+        }
+        StdOut.println();
+
+        StdOut.println("Total de cores utilizadas: " + dsatur.getColorCount());
+        StdOut.println("Coloracao valida: " + dsatur.isValidColoring());
     }
 }
